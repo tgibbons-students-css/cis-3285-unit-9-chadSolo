2cbb4b168df04fa2ca01e9159a0d0ae3a12e229d
Chad Parr <cparr2@css.edu>
2016-11-14 23:49:37 -0600

    Request  407

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -10,9 +10,10 @@ namespace SingleResponsibilityPrinciple
         static void Main(string[] args)
         {
             var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
-
+            string url = "http://faculty.css.edu/tgibbons/trades4.txt";
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
+            var tradeDataURLProvider = new URLTradeDataProvider(url);
             var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using SingleResponsibilityPrinciple.Contracts;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class URLTradeDataProvider : ITradeDataProvider
+    {
+        public StreamTradeDataProvider(string url)
+        {
+            this.url = url;
+        }
+
+        public IEnumerable<string> GetTradeData(string url)
+        {
+            //string url = "http://faculty.css.edu/tgibbons/trades4.txt";
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
